24034adade8ce5ddb6f23969dc0854b34f038fa9
joaocidade <alicegomeseberhardt@gmail.com>
2023-08-28 10:08:26 -0300

    first commit

---
+++ MyApplicationAlice/app/src/main/java/com/example/myapplicationalice/ODS3.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationalice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ODS3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ods3);
+    }
+}
